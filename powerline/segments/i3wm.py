bb54cc819f52314a23efd05c17a0f8e3df748c00
PH111P@Arch <philipwellnitz@gmx.de>
2015-05-14 14:16:44 +0200

    added mode stuff

--- powerline/segments/i3wm.py
+++ powerline/segments/i3wm.py
@@ -2,7 +2,7 @@
 from __future__ import (unicode_literals, division, absolute_import, print_function)
 
 import i3
-
+import os
 
 def calcgrp(w):
 	group = []
@@ -22,8 +22,22 @@ def workspaces(pl):
 
 	Highlight groups used: ``workspace``, ``w_visible``, ``w_focused``, ``w_urgent``
 	'''
+	if os.path.exists( '/tmp/i3_bindings_mode' ):
+	    f = open('/tmp/i3_bindings_mode', 'r')
+	    current_mode = f.readline()
+	    f.close()
+	else:
+	    current_mode = 'default'
+
 	if current_mode == 'default':
 	    return [{
 		'contents': w['name'],
 		'highlight_groups': calcgrp(w)
 	    } for w in i3.get_workspaces()]
+	else:
+	    return [{
+		'contents': current_mode,
+		'highlight_groups':['w_urgent', 'workspace']}] + [{
+		'contents': w['name'],
+		'highlight_groups': calcgrp(w)
+	    } for w in i3.get_workspaces() if w['focused'] ]
