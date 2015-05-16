71ddf0d56803e33bd3067a14c2e930980a9914ac
PH111P@Arch <philipwellnitz@gmx.de>
2015-05-16 22:04:00 +0200

    ...

--- powerline/segments/i3wm.py
+++ powerline/segments/i3wm.py
@@ -39,21 +39,16 @@ def workspaces(pl, segment_info, include_only=None):
 	global conn
 	if not conn: conn = i3ipc.Connection()
 	
-	if not include_only:
-	    return [{
-		'contents': w['name'],
-		'highlight_groups': calcgrp(w)
-	    } for w in conn.get_workspaces()]
-	else:
-	    return [{
-		'contents': w['name'],
-		'highlight_groups': calcgrp(w)
-	    } for w in conn.get_workspaces() if 'focused' in include_only and w['focused'] 
-						or 'visible' in include_only and w['visible'] 
-						or 'urgent' in include_only and w['urgent'] 
-						or 'normal' in include_only and not (w['focused'] or w['visible'] or w['urgent']) ]
+	return [{
+	    'contents': w['name'],
+	    'highlight_groups': calcgrp(w)
+	} for w in conn.get_workspaces() if not include_only 
+		or 'focused' in include_only and w['focused'] 
+		or 'visible' in include_only and w['visible'] 
+		or 'urgent' in include_only and w['urgent'] 
+		or 'normal' in include_only and not (w['focused'] or w['visible'] or w['urgent']) ]
 
-grequires_segment_info
+@requires_segment_info
 def mode(pl, segment_info, default=None):
 	'''Returns current i3 mode
 
