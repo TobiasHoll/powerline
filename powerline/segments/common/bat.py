b49d058d0f2afa3544e7df57a836bda51c9f5de2
PH111P@Arch <philipwellnitz@gmx.de>
2015-05-18 17:20:50 +0200

    added battery status

--- powerline/segments/common/bat.py
+++ powerline/segments/common/bat.py
@@ -11,6 +11,7 @@ from powerline.lib.shell import run_cmd
 # XXX Warning: module name must not be equal to the segment name as long as this 
 # segment is imported into powerline.segments.common module.
 
+show_original=False
 
 def _get_battery(pl):
 	try:
@@ -60,102 +61,62 @@ def _get_battery(pl):
 				pl.debug('Not using DBUS+UPower as no batteries were found')
 
 	if os.path.isdir('/sys/class/power_supply'):
-		linux_bat_fmt = '/sys/class/power_supply/{0}/capacity'
+		linux_bat_fmt = '/sys/class/power_supply/{0}/charge_now'
+		linux_bat_fmt1 = '/sys/class/power_supply/{0}/charge_full'
+		linux_bat_fmt2 = '/sys/class/power_supply/{0}/charge_full_design'
 		for linux_bat in os.listdir('/sys/class/power_supply'):
 			cap_path = linux_bat_fmt.format(linux_bat)
-			if linux_bat.startswith('BAT') and os.path.exists(cap_path):
+			cap_path1 = linux_bat_fmt1.format(linux_bat)
+			cap_path2 = linux_bat_fmt2.format(linux_bat)
+			if linux_bat.startswith('BAT') and os.path.exists(cap_path) and os.path.exists(cap_path1) and os.path.exists(cap_path2):
 				pl.debug('Using /sys/class/power_supply with battery {0}', linux_bat)
 
 				def _get_capacity(pl):
+					current = 0
+					full = 1
+					global show_original
 					with open(cap_path, 'r') as f:
-						return int(float(f.readline().split()[0]))
+						current = int(float(f.readline().split()[0]))
+					if not show_original:
+						with open(cap_path1, 'r') as f:
+							full = int(float(f.readline().split()[0]))
+					else:
+						with open(cap_path2, 'r') as f:
+							full = int(float(f.readline().split()[0]))
+					return int(current * 100/full)
 
 				return _get_capacity
 		pl.debug('Not using /sys/class/power_supply as no batteries were found')
 	else:
 		pl.debug('Not using /sys/class/power_supply: no directory')
 
-	try:
-		from shutil import which  # Python-3.3 and later
-	except ImportError:
-		pl.info('Using dumb “which” which only checks for file in /usr/bin')
-		which = lambda f: (lambda fp: os.path.exists(fp) and fp)(os.path.join('/usr/bin', f))
+	raise NotImplementedError
 
-	if which('pmset'):
-		pl.debug('Using pmset')
+def _get_battery_status(pl):
+	if os.path.isdir('/sys/class/power_supply'):
+		linux_bat_fmt = '/sys/class/power_supply/{0}/status'
+		for linux_bat in os.listdir('/sys/class/power_supply'):
+			cap_path = linux_bat_fmt.format(linux_bat)
+			if linux_bat.startswith('BAT') and os.path.exists(cap_path):
+				pl.debug('Using /sys/class/power_supply with battery {0}', linux_bat)
 
-		BATTERY_PERCENT_RE = re.compile(r'(\d+)%')
-
-		def _get_capacity(pl):
-			battery_summary = run_cmd(pl, ['pmset', '-g', 'batt'])
-			battery_percent = BATTERY_PERCENT_RE.search(battery_summary).group(1)
-			return int(battery_percent)
-
-		return _get_capacity
+				def _get_status(pl):
+					stat = ''
+					with open(cap_path, 'r') as f:
+						stat = f.readline().split()[0]
+					if stat == 'Unknown':
+						stat = ''
+					return stat
+				return _get_status
+		pl.debug('Not using /sys/class/power_supply as no batteries were found')
 	else:
-		pl.debug('Not using pmset: executable not found')
-
-	if sys.platform.startswith('win') or sys.platform == 'cygwin':
-		# From http://stackoverflow.com/a/21083571/273566, reworked
-		try:
-			from win32com.client import GetObject
-		except ImportError:
-			pl.debug('Not using win32com.client as it is not available')
-		else:
-			try:
-				wmi = GetObject('winmgmts:')
-			except Exception as e:
-				pl.exception('Failed to run GetObject from win32com.client: {0}', str(e))
-			else:
-				for battery in wmi.InstancesOf('Win32_Battery'):
-					pl.debug('Using win32com.client with Win32_Battery')
-
-					def _get_capacity(pl):
-						# http://msdn.microsoft.com/en-us/library/aa394074(v=vs.85).aspx
-						return battery.EstimatedChargeRemaining
-
-					return _get_capacity
-				pl.debug('Not using win32com.client as no batteries were found')
-		from ctypes import Structure, c_byte, c_ulong, byref
-		if sys.platform == 'cygwin':
-			pl.debug('Using cdll to communicate with kernel32 (Cygwin)')
-			from ctypes import cdll
-			library_loader = cdll
-		else:
-			pl.debug('Using windll to communicate with kernel32 (Windows)')
-			from ctypes import windll
-			library_loader = windll
-
-		class PowerClass(Structure):
-			_fields_ = [
-				('ACLineStatus', c_byte),
-				('BatteryFlag', c_byte),
-				('BatteryLifePercent', c_byte),
-				('Reserved1', c_byte),
-				('BatteryLifeTime', c_ulong),
-				('BatteryFullLifeTime', c_ulong)
-			]
-
-		def _get_capacity(pl):
-			powerclass = PowerClass()
-			result = library_loader.kernel32.GetSystemPowerStatus(byref(powerclass))
-			# http://msdn.microsoft.com/en-us/library/windows/desktop/aa372693(v=vs.85).aspx
-			if result:
-				return None
-			return powerclass.BatteryLifePercent
-
-		if _get_capacity() is None:
-			pl.debug('Not using GetSystemPowerStatus because it failed')
-		else:
-			pl.debug('Using GetSystemPowerStatus')
-
-		return _get_capacity
+		pl.debug('Not using /sys/class/power_supply: no directory')
 
 	raise NotImplementedError
 
-
 def _get_capacity(pl):
 	global _get_capacity
+	global show_original
 
 	def _failing_get_capacity(pl):
 		raise NotImplementedError
@@ -169,8 +130,24 @@ def _get_capacity(pl):
 		_get_capacity = _failing_get_capacity
 	return _get_capacity(pl)
 
+def _get_status(pl):
+	global _get_status
+	global show_original
 
-def battery(pl, format='{capacity:3.0%}', steps=5, gamify=False, full_heart='O', empty_heart='O'):
+	def _failing_get_status(pl):
+		raise NotImplementedError
+
+	try:
+		_get_status = _get_battery_status(pl)
+	except NotImplementedError:
+		_get_status = _failing_get_status
+	except Exception as e:
+		pl.exception('Exception while obtaining battery capacity getter: {0}', str(e))
+		_get_status = _failing_get_status
+	return _get_status(pl)
+
+
+def battery(pl, format='{capacity:3.0%}', steps=5, gamify=False, full_heart='O', empty_heart='O', original_health=False):
 	'''Return battery charge status.
 
 	:param str format:
@@ -189,6 +166,8 @@ def battery(pl, format='{capacity:3.0%}', steps=5, gamify=False, full_heart='O',
 		another gradient level and highlighting group, so it is OK for it to be 
 		the same as full_heart as long as necessary highlighting groups are 
 		defined.
+	:param bool original_health:
+		Use the original battery health ase base value. (Experimental)
 
 	``battery_gradient`` and ``battery`` groups are used in any case, first is 
 	preferred.
@@ -196,10 +175,22 @@ def battery(pl, format='{capacity:3.0%}', steps=5, gamify=False, full_heart='O',
 	Highlight groups used: ``battery_full`` or ``battery_gradient`` (gradient) or ``battery``, ``battery_empty`` or ``battery_gradient`` (gradient) or ``battery``.
 	'''
 	try:
+		global show_original
+		show_original = original_health
 		capacity = _get_capacity(pl)
 	except NotImplementedError:
 		pl.info('Unable to get battery capacity.')
 		return None
+	
+	try:
+		status = _get_status(pl)
+	except NotImplementedError:
+		pl.info('Unable to get battery status.')
+		if 'status' in format:
+			return None
+	if 'status' in format and status == '':
+		return None
+
 	ret = []
 	if gamify:
 		denom = int(steps)
@@ -220,7 +211,7 @@ def battery(pl, format='{capacity:3.0%}', steps=5, gamify=False, full_heart='O',
 		})
 	else:
 		ret.append({
-			'contents': format.format(capacity=(capacity / 100.0)),
+			'contents': format.format(capacity=(capacity / 100.0), status=status),
 			'highlight_groups': ['battery_gradient', 'battery'],
 			# Gradients are “least alert – most alert” by default, capacity has 
 			# the opposite semantics.
