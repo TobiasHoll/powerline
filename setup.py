e4a905a0074695b2d3255a287138a9de180fc47c
Philip Wellnitz <philipwellnitz@gmx.de>
2016-12-30 14:43:22 +0100

    bfs

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ else:
 
 setup(
 	name='powerline-status-i3',
-	version='1.1.7',
+	version='1.1.8',
 	description='The ultimate statusline/prompt utility. A fork containing more features for the i3 window manager.',
 	long_description=README,
 	classifiers=[
